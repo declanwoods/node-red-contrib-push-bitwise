63772d905db1cfdf6cf5baeec6532a2c0053c928
Declan Woods <docdecdoc@hotmail.com>
2021-09-08 23:36:06 +0800

    Add input node

---
+++ src/bitwise-in.js
@@ -0,0 +1,17 @@
+// const { sendUdpCommand } = require('./helpers')
+
+module.exports = function(RED) {
+  function BitwiseInNode(config) {
+    RED.nodes.createNode(this, config);
+    const configNode = RED.nodes.getNode(config.confignode)
+
+    const outputtype = config.outputtype;
+    const input = msg.input || config.input;
+
+    const ipaddress = configNode.ipaddress;
+    const port = configNode.port;
+    const command = `bwc:get:${outputtype}:${input}:`;
+  }
+
+  RED.nodes.registerType("bitwise-in", BitwiseInNode);
+}
--- src/bitwise-out.js
+++ src/bitwise-out.js
@@ -7,12 +7,12 @@ module.exports = function(RED) {
 
     this.on('input', function(msg, send, done) {
       const outputtype = config.outputtype;
-      const relay = msg.relay || config.relay;
+      const output = msg.output || config.output;
       const state = msg.state || msg.duration || config.state;
 
       const ipaddress = configNode.ipaddress;
       const port = configNode.port;
-      const command = `bwc:set:${outputtype}:${relay}:${state}:`;
+      const command = `bwc:set:${outputtype}:${output}:${state}:`;
 
       sendUdpCommand({ ipaddress, port, command })
 
